7654058a1e89648a1df57bb306ccdbcce82ebf70
don <444131125@qq.com>
2023-06-24 20:45:24 +0800

    增加提取图片边缘生成ilda文件功能

---
+++ core/imageConverter.py
@@ -0,0 +1,136 @@
+from skimage import morphology
+import numpy as np
+import os
+import cv2
+import math
+from .utils import rgb_to_ilda_color, rgb_to_7_color
+
+
+class ImageILDAConverter:
+    def __init__(self):
+        self.scales = 0.0
+        self.laser_point_interval = 1000
+        self.blank_point_interval = 500
+        self.save_image = False
+        self.skeleton = None
+        self.image = None
+        self.points = []
+        self.org_points = []
+        self.contour_use_flag = None
+        self.contour_list = None
+        pass
+
+    def convert(self, image):
+        self.image = image
+        self.scales = 65536.0 / image.shape[0]
+        self.__find_skeleton()
+        # hierarchy [后一个轮廓，前一个轮廓，父轮廓，内嵌轮廓]
+        self.contour_list, hierarchy = cv2.findContours(self.skeleton, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+        if len(self.contour_list) == 0:
+            return []
+        else:
+            contour_list_tmp = []
+            for i in range(len(self.contour_list)):
+                # if (hierarchy[0][i][3] == -1 and hierarchy[0][i][2] == -1) or hierarchy[0][i][3] >= 0:
+                contour_list_tmp.append(np.squeeze(self.contour_list[i], 1))
+            self.contour_list = contour_list_tmp
+            self.contour_use_flag = [False] * len(self.contour_list)
+        current_contour_index = 0
+        while not self.__is_all_counter_used():
+            current_contour = self.contour_list[current_contour_index]
+            for j in range(1, len(current_contour)):
+                if j == 1:
+                    self.points.append(self.__to_ilda_point(current_contour[j - 1], True))
+                points = self.__get_points_between(current_contour[j - 1], current_contour[j])
+                self.points += points
+            # for i in range(5):
+            #     self.points.append(self.__to_ilda_point(current_contour[-1], True))
+            next_idx = self.__find_nearest_contour(current_contour_index)
+            if next_idx is not None:
+                self.points.append(self.__to_ilda_point(current_contour[-1], False))
+                blank_points = self.__get_points_between(current_contour[-1], self.contour_list[next_idx][0],
+                                                         False)
+                self.points += blank_points
+                current_contour_index = next_idx
+        # 增加运动点位返回起点
+        if current_contour_index != 0:
+            blank_points = self.__get_points_between(self.contour_list[current_contour_index][-1],
+                                                     self.contour_list[0][0],
+                                                     False)
+            self.points += blank_points
+        point_image = np.zeros(image.shape)
+        for i in self.org_points:
+            point_image[i[0]][i[1]] = rgb_to_7_color(self.image[i[0]][i[1]])
+            # print(point_image[i[0]][i[1]])
+        self.__output_img("point_image", point_image, img_type=".bmp")
+        return self.points
+
+    def __find_nearest_contour(self, contour_index):
+        min_dis = 99999999.0
+        last_point = self.contour_list[contour_index][-1]
+        nearest_idx = None
+        self.contour_use_flag[contour_index] = True
+        for j in range(len(self.contour_list)):
+            first_point = self.contour_list[j][0]
+            if not self.contour_use_flag[j]:
+                dis = math.sqrt(
+                    math.pow(last_point[0] - first_point[0], 2) + math.pow(last_point[1] - first_point[1], 2))
+                if dis < min_dis:
+                    nearest_idx = j
+                    min_dis = dis
+        # self.contour_use_flag[nearest_idx] = True
+        return nearest_idx
+
+    def __is_all_counter_used(self):
+        for i in self.contour_use_flag:
+            if not i:
+                return False
+        return True
+
+    def __get_points_between(self, point1, point2, enable=True):
+        points = []
+        dis = math.sqrt(math.pow(point1[0] - point2[0], 2) + math.pow(point1[1] - point2[1], 2)) * self.scales
+        if enable:
+            middle_points_count = round(dis / self.laser_point_interval) - 1
+        else:
+            middle_points_count = round(dis / self.blank_point_interval) + 3
+        if middle_points_count > 0:
+            x_interval = (point2[0] - point1[0]) / (middle_points_count + 1)
+            y_interval = (point2[1] - point1[1]) / (middle_points_count + 1)
+            for i in range(middle_points_count):
+                x_middle = point1[0] + x_interval * i
+                y_middle = point1[1] + y_interval * i
+                points.append(self.__to_ilda_point([x_middle, y_middle], enable))
+        points.append(self.__to_ilda_point(point2, enable))
+        return points
+
+    def __to_ilda_point(self, point, enable):
+        x = int(point[0] * self.scales) - 32768
+        #  y 反向
+        y = -(int(point[1] * self.scales) - 32768)
+        color = self.image[int(point[1]), int(point[0])]
+        color_bgr = [color[2], color[1], color[0]]
+        color = rgb_to_ilda_color(color_bgr)
+        self.org_points.append([int(point[1]), int(point[0])])
+        return [x, y, 0, color, enable]
+
+    def __find_skeleton(self):
+        gray_image = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+        # cv2.imshow("gary", gray_image)
+        ret, binary = cv2.threshold(gray_image, 240, 255, cv2.THRESH_BINARY_INV)
+        # cv2.imshow("binary", binary)
+        skeleton0 = morphology.skeletonize(binary)
+        self.skeleton = skeleton0.astype(np.uint8) * 255
+        self.__output_img("skeleton", self.skeleton)
+
+        color_edge_image = cv2.bitwise_and(self.image, self.image, mask=self.skeleton)
+        self.__output_img("edge_image", color_edge_image)
+        pass
+
+    def __output_img(self, name, img, img_type=".jpg"):
+        if self.save_image:
+            dir_path = os.path.dirname(os.path.abspath(__file__)) + "\\output\\"
+            if not os.path.exists(dir_path):
+                os.makedirs(dir_path)
+            # print(dir_path + name + ".jpg")
+            cv2.imwrite(dir_path + name + img_type, img)
---
+++ core/lidaGenerator.py
@@ -0,0 +1,123 @@
+from typing import List
+
+
+class LidaPoints:
+    def __init__(self):
+        self.BYTE_ORDER = "big"
+        self.x = 0
+        self.y = 0
+        self.z = 0
+        self.enable = 0
+        self.color = 0
+        pass
+
+    def to_bytes(self):
+        point_bytes = bytearray(8)
+        point_bytes[0:2] = self.x.to_bytes(2, self.BYTE_ORDER, signed=True)
+        point_bytes[2:4] = self.y.to_bytes(2, self.BYTE_ORDER, signed=True)
+        point_bytes[4:6] = self.z.to_bytes(2, self.BYTE_ORDER, signed=True)
+        point_bytes[6] = self.enable
+        point_bytes[7] = self.color
+        return point_bytes
+
+
+class LidaHeader:
+    def __init__(self):
+        self.BYTE_ORDER = "big"
+        self.ENCODING = "utf-8"
+        self.LIDA = b"ILDA"  # 4 byte
+        self.type = 0  # 4byte
+        self.name = ""  # 8byte
+        self.company = ""  # 8byte
+        self.frame_length = 0  # 2byte
+        self.frame_index = 0  # 2byte
+        self.total_frame = 0  # 2byte
+        self.head = 0  # 1byte
+        self.end = 0  # 1byte
+
+    def to_bytes(self):
+        header_bytes = bytearray(32)
+        header_bytes[0:4] = self.LIDA
+        header_bytes[4:8] = self.type.to_bytes(4, self.BYTE_ORDER)
+        header_bytes[8:16] = bytes(self.name[0:8], self.ENCODING)
+        header_bytes[15] = 0x20
+        header_bytes[16:24] = bytes(self.company[0:8], self.ENCODING)
+        header_bytes[23] = 0x20
+        header_bytes[24:26] = self.frame_length.to_bytes(2, self.BYTE_ORDER)
+        header_bytes[26:28] = self.frame_index.to_bytes(2, self.BYTE_ORDER)
+        header_bytes[28:30] = self.total_frame.to_bytes(2, self.BYTE_ORDER)
+        header_bytes[30:32] = [0x00, 0x00]
+        return header_bytes
+
+
+class LidaFrame:
+    def __init__(self):
+        self.header: LidaHeader = LidaHeader()
+        self.points: List[LidaPoints] = []
+
+    def to_bytes(self):
+        frame_bytes = bytearray()
+        frame_bytes += self.header.to_bytes()
+        for point in self.points:
+            frame_bytes += point.to_bytes()
+        return frame_bytes
+
+
+class LidaFile:
+
+    def __init__(self):
+        self.frames: List[LidaFrame] = []
+        self.cur_frame_index = None
+        self.name = ""
+        self.company = ""
+        self.end = bytes(
+            [0x49, 0x4C, 0x44, 0x41, 0x00, 0x00, 0x00, 0x00,
+             0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
+             0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
+             0x00, 0x00, 0x00, 0x00, 0x00, 0x3D, 0x00, 0x00])
+        pass
+
+    def to_bytes(self):
+        self.refresh_frame_header()
+        file_bytes = bytearray()
+        i = 0
+        for frame in self.frames:
+            frame.header.name = str(i).zfill(3) + self.name
+            frame.header.company = self.company
+            file_bytes += frame.to_bytes()
+            i += 1
+        file_bytes += self.end
+        return file_bytes
+
+    def add_point(self, x, y, z, color, enable: bool = True):
+        point = LidaPoints()
+        point.x = x
+        point.y = y
+        point.z = z
+        if enable:
+            point.enable = 0
+            point.color = color
+            pass
+        else:
+            # 0x40 disable
+            point.enable = 0x40
+            point.color = 0
+        self.frames[self.cur_frame_index].points.append(point)
+        pass
+
+    def new_frame(self):
+        frame = LidaFrame()
+        self.frames.append(frame)
+        if self.cur_frame_index is None:
+            self.cur_frame_index = 0
+        else:
+            self.cur_frame_index += 1
+        pass
+
+    def refresh_frame_header(self):
+        total_frames = len(self.frames)
+        for i in range(total_frames):
+            frame = self.frames[i]
+            frame.header.frame_index = i
+            frame.header.frame_length = len(frame.points)
+            frame.header.total_frame = total_frames
