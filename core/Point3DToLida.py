6c1fc0bca1bd47657fd79efeffbcd27aaa13f80a
ldj <444131125@qq.com>
2023-11-06 23:00:00 +0800

    新点位投影

--- core/Point3DToLida.py
+++ core/Point3DToLida.py
@@ -25,6 +25,7 @@ class Point3DToLida(ImageILDAConverter):
         self.y_angle_interval = [-16.7, 16.7]
         self.point_max = 65535
         self.lida_file = LidaFile()
+        self.convert_type = "camera"
         self.scales = 1.0
         self.contour_list = []
         self.point_add_strategy: PointAddInterface = PointAddInterface()
@@ -51,6 +52,27 @@ class Point3DToLida(ImageILDAConverter):
         pass
 
     def point3d_to_image_point(self, point):
+        if self.convert_type == "camera":
+            return self.point3d_to_image_point_camera(point)
+        if self.convert_type == "origin":
+            return self.point3d_to_image_point_origin(point)
+
+    def point3d_to_image_point_origin(self, point):
+        x, y = 0, 0
+        if self.camera_trans_mtx is not None:
+            actual_point = np.dot(self.camera_trans_mtx, point)
+            actual_point = np.dot(self.camera_project_trans_mtx, actual_point)
+            x, y = actual_point[0], actual_point[1]
+            alpha, beta = point_to_angle(actual_point[0:3])
+            if alpha is not None:
+                x = int(alpha / self.x_angle_interval[1] * int(self.point_max / 2)) + int(self.point_max / 2)
+            if beta is not None:
+                # 投影仪二维右手系转图像二维左手系
+                y = int(beta / self.y_angle_interval[1] * int(self.point_max / 2)) + int(self.point_max / 2)
+            print("x,y=", x, y)
+        return [x, y]
+
+    def point3d_to_image_point_camera(self, point):
         x, y = 0, 0
         if self.camera_trans_mtx is not None:
             actual_point = np.dot(self.camera_trans_mtx, point)
@@ -65,7 +87,7 @@ class Point3DToLida(ImageILDAConverter):
             # if beta is not None:
             #     # 投影仪二维右手系转图像二维左手系
             #     y = int(beta / self.y_angle_interval[1] * int(self.point_max / 2)) + int(self.point_max / 2)
-            print(x, y)
+            print("x,y=", x, y)
         return [x, y]
 
     def new_frame(self, duration=0):
@@ -75,12 +97,13 @@ class Point3DToLida(ImageILDAConverter):
         self.lida_file.company = "buz141"
         self.lida_file.new_frame()
         self.lida_file.frames[self.lida_file.cur_frame_index].header.duration = duration
-        print(len(points))
+        print("frame point length= ", len(points))
         for i in range(len(points)):
             point = points[i]
             self.lida_file.add_point(point[0], point[1], point[2], point[3], point[4])
         self.contour_list = []
         self.points = []
+        self.org_points = []
 
     def add_image(self, image):
         points = self.convert(image)
--- test/3dLidaTest.py
+++ test/3dLidaTest.py
@@ -7,8 +7,7 @@ import serial
 import cv2
 
 
-def \
-        generate_point3d_lida(points, trans_mat, color):
+def generate_point3d_lida(points, trans_mat, color):
     converter = Point3DToLida()
     converter.camera_project_trans_mtx = np.array([[1, 0, 0, 0],
                                                    [0, 1, 0, 0],
@@ -17,51 +16,61 @@ def \
     image1 = cv2.imread("C:\\Users\\Administrator\\Pictures\\527NKJ84.png")
     image2 = cv2.imread("C:\\Users\\Administrator\\Pictures\\00328.png")
     image3 = cv2.imread("C:\\Users\\Administrator\\Pictures\\506K8.png")
-    converter.new_frame(duration=3)
-    converter.add_image(image1)
+    image_list = [image1, image2, image3, image3]
+    converter.convert_type = "origin"
+    # converter.new_frame(duration=3)
+    # converter.add_image(image1)
 
     converter.camera_trans_mtx = trans_mat
     converter.set_strategy(CirclePointStrategy())
-    for i in range(len(points)):
-        converter.add_point(points[i], np.array([0, 255, 0]))
-    converter.new_frame(duration=3)
-
-    points1 = points[np.where((color == [255, 255, 0]).all(axis=-1))]
-    color1 = color[np.where((color == [255, 255, 0]).all(axis=-1))]
-    for i in range(len(points1)):
-        converter.add_point(points1[i], color1[i])
-    converter.new_frame(duration=3)
-
-    points2 = points[np.where((color == [255, 255, 255]).all(axis=-1))]
-    color2 = color[np.where((color == [255, 255, 255]).all(axis=-1))]
-    for i in range(len(points2)):
-        converter.add_point(points2[i], color2[i])
-    converter.new_frame(duration=3)
+    for i, point_group in enumerate(points):
+        converter.new_frame(duration=3)
+        converter.add_image(image_list[i])
+        converter.new_frame(duration=3)
+        for j, point in enumerate(point_group):
+            converter.add_point(point, color[i])
+        # converter.new_frame(duration=3)
+    # for i in range(len(points)):
+    #     converter.add_point(points[i], np.array([0, 255, 0]))
+    # converter.new_frame(duration=3)
+    #
+    # points1 = points[np.where((color == [255, 255, 0]).all(axis=-1))]
+    # color1 = color[np.where((color == [255, 255, 0]).all(axis=-1))]
+    # for i in range(len(points1)):
+    #     converter.add_point(points1[i], color1[i])
+    # converter.new_frame(duration=3)
+    #
+    # points2 = points[np.where((color == [255, 255, 255]).all(axis=-1))]
+    # color2 = color[np.where((color == [255, 255, 255]).all(axis=-1))]
+    # for i in range(len(points2)):
+    #     converter.add_point(points2[i], color2[i])
+    # converter.new_frame(duration=3)
 
     return converter.to_bytes()
     pass
 
 
 if __name__ == "__main__":
-    target_point = np.array([
-        [552, 13134.5, 1567.599], [552, 13134.5, 1540.034], [552, 13134.5, 1507.469], [552, 13134.5, 1473.904],
-        [552, 13134.5, 1441.339], [552, 13134.5, 1406.774], [552, 13134.5, 1373.209], [552, 13134.5, 1339.644],
-        [552, 13134.5, 1306.079], [552, 13134.5, 1272.514], [552, 13134.5, 1243.949],
-        [552, 13098.7, 1247.449], [552, 13098.7, 1278.497], [552, 13098.7, 1312.045], [552, 13098.7, 1345.512],
-        [552, 13098.7, 1379.14], [552, 13098.7, 1412.687], [552, 13098.7, 1448.235], [552, 13098.7, 1479.782],
-        [552, 13098.7, 1513.33], [552, 13098.7, 1545.877], [552, 13098.7, 1573.425]])
-    color = np.array([[255, 255, 0], [255, 255, 0],
-                      [255, 255, 255], [255, 255, 255], [255, 255, 255], [255, 255, 255],
-                      [255, 255, 255], [255, 255, 255], [255, 255, 255], [255, 255, 255],
-                      [255, 255, 255], [255, 255, 255], [255, 255, 255], [255, 255, 255],
-                      [255, 255, 255], [255, 255, 255], [255, 255, 255], [255, 255, 255],
-                      [255, 255, 255], [255, 255, 255], [255, 255, 0], [255, 255, 0]])
-    trans_mat = np.linalg.inv(np.array([[0, 0, 1, -300.8],
-                                        [0, -1, 0, 12720.053],
-                                        [1, 0, 0, 1628.281],
+    target_point = np.array([[[1099.311599, 8953, -197.149], [1095.643198, 8953, -127.152],
+                              [1096.691784, 8953, -147.16], [1097.740371, 8953, -167.168]],
+                             [[1100.780763, 8953, -225.182]],
+                             [[1107.41044, 8951, -256.147], [1110.225428, 8960.5, -300.307],
+                              [1111.452588, 8949, -323.722], [1112.679748, 8960.5, -347.138],
+                              [1113.906908, 8949, -370.554], [1115.134068, 8960.5, -393.969],
+                              [1116.361228, 8949, -417.385], [1117.643129, 8960.5, -441.845],
+                              [1118.703853, 8949, -462.085], [1119.6459, 8960.5, -480.06],
+                              [1120.920701, 8949, -504.385], [1122.195501, 8960.5, -528.709],
+                              [1123.470301, 8949, -553.034], [1124.745101, 8960.5, -577.359],
+                              [1126.028374, 8949, -601.845], [1126.788502, 8960.5, -616.349],
+                              [1127.54863, 8949, -630.853], [1128.253042, 8960.5, -653.848]],
+                             [[1126.539384, 8959, -678.471]]], dtype=object)
+    color = np.array([[255, 255, 0], [255, 0, 0], [0, 255, 0], [0, 0, 255]])
+    trans_mat = np.linalg.inv(np.array([[0, 0, 1, 2749.78],
+                                        [0, -1, 0, 7205.31],
+                                        [1, 0, 0, -819.484],
                                         [0, 0, 0, 1]]))
-    rotate_angle_x = -26 / 180 * math.pi
-    rotate_angle_y = 12 / 180 * math.pi
+    rotate_angle_x = 47.88 / 180 * math.pi
+    rotate_angle_y = 10.714 / 180 * math.pi
     rotate_mat_y = np.array([[math.cos(rotate_angle_y), 0, math.sin(rotate_angle_y), 0],
                              [0, 1, 0, 0],
                              [-math.sin(rotate_angle_y), 0, math.cos(rotate_angle_y), 0],
@@ -72,8 +81,9 @@ if __name__ == "__main__":
                              [0, 0, 0, 1]])
     trans_mat = np.dot(rotate_mat_x, trans_mat)
     trans_mat = np.dot(rotate_mat_y, trans_mat)
-    b = [[1.0]] * len(target_point)
-    target_point = np.concatenate((target_point, b), 1)
+    for i, point_group in enumerate(target_point):
+        b = [[1.0]] * len(point_group)
+        target_point[i] = np.concatenate((point_group, b), 1)
     file_bytes = generate_point3d_lida(target_point, trans_mat, color)
     file = open("../product.ild", 'wb')
     file.write(file_bytes)
